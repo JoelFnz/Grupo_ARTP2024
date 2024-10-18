c6394e59d50d86350170c9c488195b81467e62f6
GabrielOmarRomero <romerogabrielomar1@gmail.com>
2024-10-17 22:25:42 -0300

    Notificación de Recordar Usuario
    Se le indicará al usuario, por medio de una notificación, que su cuenta será recordada para la próxima vez que inicie sesión.

--- app/src/main/java/com/example/grupoar_tp2024/activities/Login.kt
+++ app/src/main/java/com/example/grupoar_tp2024/activities/Login.kt
@@ -1,6 +1,11 @@
 package com.example.grupoar_tp2024.activities
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -11,6 +16,8 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.grupoar_tp2024.bd.AppDatabase
@@ -25,18 +32,32 @@ import com.example.grupoar_tp2024.apiRest.ResultadoDTO
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import android.Manifest
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class Login : AppCompatActivity() {
 
     val api = RetrofitClient.retrofit.create(IPokemonApi::class.java)
 
+    companion object {
+        const val CHANNEL_ID = "user_channel"
+        const val REQUEST_CODE = 1001
+    }
+
+    private val titleNotification = "Usuario Recordado"
+    private val contentNotification = "Su Usuario será recordado para la próxima vez que inicie sesión."
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //LIBERADOR RECORDAR USUARIO
-        /* val preferenciasLimpiar = getSharedPreferences(resources.getString(R.string.sp_credenciales), MODE_PRIVATE)
+
+        val preferenciasLimpiar = getSharedPreferences(resources.getString(R.string.sp_credenciales), MODE_PRIVATE)
         with(preferenciasLimpiar.edit()) {
             clear()
             apply()
-        } */
+        }
+
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -46,6 +67,11 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //CANAL DE NOTIFICACIONES
+        createNotificationChannel()
+        //PERMISO DEL USUARIO PARA LAS NOTIFICACIONES
+        checkNotificationPermission()
 
         //Toolbar
         val toolbar: Toolbar =findViewById(R.id.toolbar)
@@ -90,11 +116,15 @@ class Login : AppCompatActivity() {
                 if (cbRecordar.isChecked) {
                     preferencias.edit().putString(resources.getString(R.string.nombre_usuario), email).apply()
                     preferencias.edit().putString(resources.getString(R.string.password_usuario), password).apply()
+
+                    sendNotification(titleNotification, contentNotification)
                 }
                 startMainActivity(email)
             } else
                 Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
         }
+
+
     }
 
     private fun startMainActivity(email: String) {
@@ -104,4 +134,57 @@ class Login : AppCompatActivity() {
         finish()
     }
 
+    //CANAL DE NOTIFICACIONES
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelId = CHANNEL_ID
+            val channelName = "Recordar Usuario"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = "Notificaciones para recordar usuario"
+            }
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    //NOTIFICACION
+    private fun sendNotification(title: String, content: String){
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_catching_pokemon_24)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setColor(ContextCompat.getColor(this, R.color.rojoPokebola))
+            .setStyle(NotificationCompat.BigTextStyle().bigText(content))
+
+        with(NotificationManagerCompat.from(this)){
+            if (ActivityCompat.checkSelfPermission(
+                    this@Login,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notify(1, notificationBuilder.build())
+        }
+    }
+
+    //PARA QUE EL USUARIO PERMITA LAS NOTIFICACIONES
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), REQUEST_CODE)
+            }
+        }
+    }
+
 }
+
